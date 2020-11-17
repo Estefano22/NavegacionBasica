fe6a9f1193681c7f595176317cc1f6c29d9b757b
estefano <sandulescustefano8@gmail.com>
2020-11-17 21:12:53 +0100

    Actualizado

--- app/src/main/java/com/example/navegacionbasica/MainActivity2.kt
+++ app/src/main/java/com/example/navegacionbasica/MainActivity2.kt
@@ -5,18 +5,11 @@ import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity2 : AppCompatActivity() {
-    companion object {
-        const val botonActivity2 = "Imagen"
-    }
-        override fun onCreate(savedInstanceState: Bundle?) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.segundaactivity_main)
             val Imagen = findViewById<ImageView>(R.id.ic_logo)
-
-            val src = intent.getStringExtra(botonActivity2)
-
-            src?.let {
                 Imagen.setImageResource(R.drawable.ic_logo)
             }
         }
-}
